47d48028e0cb717c8be44eb8f5db0c2adcdad2c7
Shivam Singh <shivam.singh@bigbluepeople.com>
2023-02-23 16:35:16 +0530

    Initial commit

---
+++ coercion.js
@@ -0,0 +1,5 @@
+//As the name implies, type conversion is the process of converting a value from one type to another.
+// Values in JavaScript can be of different types. You could have a number, string, object, boolean – you name it. Sometimes, you may want to convert data from one type to another to fit a certain operation.
+// Type conversion can either be implicit (automatically done during code execution) or explicit (done by you the developer).
+
+// Implicit Type Conversion is also known (and more commonly referred to) as Coercion while Explicit Type Conversion is also known as Type Casting.
